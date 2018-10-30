bc7024c106d206dad7a2c9c8affd44ed48b899aa
vagrant <vagrant@homestead>
2018-10-30 23:54:31 +0000

    added ajax call in orders.index view to implement the takeOrder feature, added new route for the ajax call, reconstruct the logic in orders.index view to display proper orders in each list

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -3,25 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log; // this is for writting logging messages to storage/logs
+use Illuminate\Support\Facades\Log; // this is for writting logging messages to storage/logs. use it like Log::alert('goes through takeOrder action in orderController');
 
 use App\Order;
+use Response;
 
 class OrdersController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display available orders (exclued currently logged in user).
+     * Display orders that were created by currently logged in user.
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        $orders = Order::all();
-        $user = null;
-        if(auth()->user() !== null) {
-          $user = auth()->user();
-        }
-        return view('orders.index')->with(['user'=>$user, 'orders' => $orders]);
+      if(auth()->user() === null) {
+        // if gets in this statement meaning the user is not logged in
+        return redirect('/')->with('error', 'You need to login in order to view/create order.'); 
+      }
+      $orders = Order::all(); // this is for the debugging purpose, to print all records of order
+      $user = null;
+      if(auth()->user() !== null) {
+        $user = auth()->user();
+      }
+      $availableOrders = Order::where('owner', '!=' , $user->name)->whereNull('taker')->get();
+      $ordersFromCurrentUsers = Order::where('owner', $user->name)->get();
+      return view('orders.index')->with(['user'=>$user, 'availableOrders'=> $availableOrders,'ordersFromCurrentUsers' => $ordersFromCurrentUsers, 'orders' => $orders]);
     }
 
     /**
@@ -42,14 +50,12 @@ class OrdersController extends Controller
      */
     public function store(Request $request)
     {
-//        Order::create($request->all());
-
         // we also need to all current user's id and address here (address can be taken from form as well)
         $request->validate([
           'title' => 'required',
           'item' => 'required',
           'address' => 'required',
-          'price' => 'required',
+          'price' => 'required'
         ]);
         
         $order = new Order;
@@ -124,4 +130,19 @@ class OrdersController extends Controller
         $order->delete();
         return redirect('/orders');
     }
+
+
+    /**
+     * assign the specified order to the currently logged in user.
+     * this action is for responding to the ajax call from orders.index view
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function takeOrder($id)
+    {
+        $user = auth()->user();
+        $order = Order::where('id', $id)->update(['taker'=> $user->name]);
+        return \Response::json(['msg' => 'taken'], 200);
+    }
+
 }
--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="row">
-  <div class="col-md-8">
+  <div class="col-md-7">
     <div class="row">
       <div class="col-md-12">
         Area reserved for google map api
@@ -21,26 +21,42 @@
     </div>
     
   </div>
-  <div class="col-md-4">
+  <div class="col-md-5">
     @if ($user !== null)
       <h3>All available orders</h3>
-          <h6>TODO: This Page should display all orders submitted by the current user</h6>
-          <ul>
-              @foreach($orders as $order)
-                <li>
-                  <a href="{{route('orders.show', $order->id)}}">{{$order->title}}</a>
+        {{-- <h6>TODO: This Page should display all orders submitted by the current user</h6> --}}
+        @if (count($availableOrders) > 0)
+          <ul id="listOfTakeButtons">
+            @foreach($availableOrders as $availableOrder)
+              <li>
+                {{-- <a href="{{route('orders.show', $order->id)}}">{{$order->title}}</a>
                 <span>Order owner: {{$order->owner}}</span>
-                </li>
-              @endforeach
+                @if ($order->owner !== $user->name)
+                <a href="" class="btn btn-default">
+                <button class="btn btn-primary" id="{{$order->id}}">Take</button>
+                </a>
+                @endif --}}
+                <span>Order title: <b>{{$availableOrder->title}}</b> </span><br>
+                <span>Order owner: <b>{{$availableOrder->owner}}</b> </span>
+                <a class="btn btn-default">
+                  <button class="btn btn-primary" id="{{$availableOrder->id}}">Take</button>
+                </a>
+              </li>
+            @endforeach
           </ul>
+        @else
+        <h5>You are late~ No available order.</h5>
+        @endif
+          
       <hr style="border-top: 3px solid rgba(0,0,0,.1);">  
       <h3>Orders you have posted</h3>
-      @if (count($orders) > 0)
+      @if (count($ordersFromCurrentUsers) > 0)
         <ul>
-            @foreach($orders as $order)
-              @if ($order->owner === $user->name)
+            {{-- this is for displaying the orders that are created by the currently logged in user --}}
+            @foreach($ordersFromCurrentUsers as $ordersFromCurrentUser)
+              @if ($ordersFromCurrentUser->owner === $user->name)
                 <li>
-                  <a href="{{route('orders.show', $order->id)}}">{{$order->title}}</a>
+                  <a href="{{route('orders.show', $ordersFromCurrentUser->id)}}">{{$ordersFromCurrentUser->title}}</a>
                 </li>
               @endif
             @endforeach
@@ -54,5 +70,62 @@
     </div>
     @endif
 </div>
+
+<div class="row" style="width: 100%; margin-top: 30px;">
+  <div class="col-md-12">
+    <table style="width: 100%;">
+      <h4>The following table is solely for debugging purpose.</h4>
+      <h4>When we do final demo, this table will be removed.</h4>
+      <tr>
+        <th>order id</th>
+        <th>title</th>
+        <th>item</th>
+        <th>owner</th>
+        <th>taker</th>
+      </tr>
+      @foreach ($orders as $order)
+      <tr>
+        <td style="margin-right: 5px;">{{$order->id}}</tds>
+        <td>{{$order->title}}</td>
+        <td style="margin-right: 5px;">{{$order->item}}</td>
+        <td style="margin-right: 5px;">{{$order->owner}}</td>
+        <td style="margin-right: 5px;">{{$order->taker}}</td>
+      </tr>
+      @endforeach
+    </table>
+  </div>
+</div>
     
 @endsection
+<script
+  src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+  integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E="
+  crossorigin="anonymous">
+</script>
+<script>
+  $(document).ready(function(){
+    $("#listOfTakeButtons button").click(function(e){
+      e.preventDefault(); 
+      let orderId = this.id; // first, get the id of the order, which is wriiten as ID of the button element
+      $.ajaxSetup({
+        headers: {
+          'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+      });
+      $.ajax({
+          type: 'post',
+          url: `orders/take/${orderId}`,
+          success: function(msg) {
+            console.log('ajax take order success'); 
+            alert('You have taken the order successfully');
+            window.location.reload(true); 
+          },
+          error: function(msg) {
+            alert('Fail to take the order successfully');
+            console.log('ajax call to takeOrder action in order controller error ', msg);
+            window.location.reload(true); 
+          }
+      });
+    });
+});
+</script>
