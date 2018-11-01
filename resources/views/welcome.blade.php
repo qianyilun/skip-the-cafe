1b7dcef1eb246c7a0f466da7077c9b6a5815a20b
Andrew <yya123@sfu.ca>
2018-10-31 20:31:50 -0700

    Removed redundant login and register link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,17 +3,6 @@
 @section('content')
     <div class="container">
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
             <div class="content">
                 <div style="text-align: left;" class="question">
                     Time is cheap?
