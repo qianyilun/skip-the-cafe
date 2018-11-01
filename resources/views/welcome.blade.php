08fca993b81550f8b0b343bfca1a3500fa63f48b
Andrew <yya123@sfu.ca>
2018-10-31 21:10:32 -0700

    UI Improvements for home page and create order page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,12 +10,12 @@
                 <div class="title m-b-md">
                     Skip the Cafe
                 </div>
-                <div style="text-align: right;" class="slogan">
-                    Switch "Delivery" | "Receiver" anytime, anywhere.
+                <div class="slogan">
+                    <p>Switch "Delivery" | "Receiver" anytime, anywhere.</p>
                     <br>
-                    <a href="{{route('orders.index')}}">View All Orders</a>
+                    <a class="btn btn-primary btn-lg btn-block" href="{{route('orders.index')}}">View All Orders</a>
                     <br>
-                    <a href="{{route('orders.create')}}">New Order</a>
+                    <a class="btn btn-primary btn-lg btn-block" href="{{route('orders.create')}}">New Order</a>
                 </div>
             </div>
         </div>
