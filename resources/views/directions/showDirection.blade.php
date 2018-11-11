d3c47fa964f48228f819e43ddfa5f9e8c612aca7
jyl49 <jyl49@sfu.ca>
2018-11-11 23:40:29 +0000

    added a snippet of code to display text version of the routes and added css on it to make it scrollable

--- resources/views/directions/showDirection.blade.php
+++ resources/views/directions/showDirection.blade.php
@@ -1,14 +1,42 @@
 @extends('layouts.app')
+<style>
+  #overlay {
+    position: absolute;
+    width: 100%;
+    height: 450px;
+    background: black;
+    opacity: .8;
+    top: 0;
+    left: 0;
+    overflow: auto;
+}
+#overlayContent {
+    color: white;
+    padding: 10px 20px;
+}
+</style>
 
 @section('content')
-
-<h3>show direction page ID: {{$id}}</h3>
-<h3>show direction page orderLongitude: {{$orderLongitude}}</h3>
-<h3>show direction page orderLatitude: {{$orderLatitude}}</h3>
-<h3>show direction page currentUserlongitude: {{$currentUserlongitude}}</h3>
-<h3>show direction page currentUserlatitude: {{$currentUserlatitude}}</h3>
+<h3>order ID: {{$id}}</h3>
+<h3>orderLongitude: {{$orderLongitude}}</h3>
+<h3>orderLatitude: {{$orderLatitude}}</h3>
+<h3>currentUserlongitude: {{$currentUserlongitude}}</h3>
+<h3>currentUserlatitude: {{$currentUserlatitude}}</h3>
 <button class="btn btn-primary">Complete order, notify the order owner</button>
-<div id='map' style="height: 500px; width: 100%;"></div>
+<div class="row">
+  <div class="col-md-3">
+    <div id="overlay">
+        <div id="overlayContent">
+          <div id='instructions'></div>
+        </div>
+    </div>
+  </div>
+
+  <div class="col-md-9">
+    <div id='map' style="height: 500px; width: 100%;"></div>
+  </div>
+</div>
+
 @endsection
 <script
   src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
@@ -25,7 +53,7 @@
     var currentUserlongitude = {{$currentUserlongitude}};
     var currentUserlatitude = {{$currentUserlatitude}};
     const accessToken = 'pk.eyJ1Ijoia2Vsdmlua2tsIiwiYSI6ImNqb2RhemVvbTB3Mmoza24yMHFlamc4ZG4ifQ.9YopsiI9D9r2PSKjb7nhgQ';
-    var directionsRequest = 'https://api.mapbox.com/directions/v5/mapbox/walking/'+ currentUserlongitude + ',' + currentUserlatitude + ';' + orderLongitude + ',' + orderLatitude + '?geometries=geojson&access_token=' + accessToken;
+    var directionsRequest = 'https://api.mapbox.com/directions/v5/mapbox/walking/'+ currentUserlongitude + ',' + currentUserlatitude + ';' + orderLongitude + ',' + orderLatitude + '?steps=true&geometries=geojson&access_token=' + accessToken;
     var start = [currentUserlongitude, currentUserlatitude]; // for the marker to be placed on the map
     var end = [orderLongitude, orderLatitude]; // for the marker to be placed on the map
     
@@ -86,8 +114,16 @@
             }
           }
         });
+        // added slightly modified version of routes instructions
+        var instructions = document.getElementById('instructions');
+        var steps = data.routes[0].legs[0].steps;
+        steps.forEach(function(step) {
+          instructions.insertAdjacentHTML('beforeend', '<p>' + step.maneuver.instruction + '</p>');
+        });
       });
       
     });
+
+    
   });
 </script>
