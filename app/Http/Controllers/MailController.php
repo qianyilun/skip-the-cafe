b833d922f7409bbbc0b68bf5f57f52a12ec2b164
Allen Qian <qianyilun@gmail.com>
2018-11-11 22:30:20 -0800

    add feature of sending email when user create a new order

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\MyMail;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+
+class mailController extends Controller
+{
+    /**
+     * @param $orderTitle
+     */
+
+    public function sendEmailWhenCreateNewOrder($orderTitle) {
+        $userId = auth()->user()->id;
+        $user = DB::table('users')->where('id', "$userId")->first();
+
+        $sendTo = $user->email;
+        $userName = $user->name;
+        $data = [
+            'orderTitle' => $orderTitle
+        ];
+
+        Mail::send('emails.new_order', $data, function($message) use ($sendTo, $userName){
+            $message->to($sendTo, $userName)->subject('You order is posted successfully');
+        });
+    }
+
+    /**
+     * A sample function for sending a hardcode email when necessary
+     */
+    public function send() {
+        $data = [
+           'title' => 'Order submitted and posted',
+           'content' => 'This is content'
+        ];
+        Mail::send('emails.test', $data, function($message) {
+            $message->to('qianyiluntemp@gmail.com', 'yilun qian TEST')->subject('hey tester');
+        });
+    }
+}
---
+++ resources/views/emails/new_order.blade.php
@@ -0,0 +1,7 @@
+Hi {{ Auth::user()->name }},<br>
+<br>
+Your order <b>{{$orderTitle}}</b> has been successfully posted. <br>
+Please click <a href="http://skipthecafe.test/orders">here</a> to view your order. <br>
+<br>
+Thanks,<br>
+Skip-the-Cafe
