bcaf43b7004265e865c029e945cbc2d7e2bce400
Allen Qian <qianyilun@gmail.com>
2018-11-13 22:06:06 -0800

    add email message of order completed

---
+++ resources/views/emails/delivery_order.blade.php
@@ -0,0 +1,7 @@
+Hi {{$userName}},<br>
+<br>
+Your order <b>{{$orderTitle}}</b> has been delivered successfully by {{Auth::user()->name}}. <br>
+Please click <a href="http://skipthecafe.test/orders">here</a> to view your order. <br>
+<br>
+Thanks,<br>
+Skip-the-Cafe
