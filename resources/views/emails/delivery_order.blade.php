0e67fe082c8c58e964c9fcc40d6071a1da986f71
Allen Qian <qianyilun@gmail.com>
2018-11-14 20:32:51 -0800

    move email's href link to .env file

--- resources/views/emails/delivery_order.blade.php
+++ resources/views/emails/delivery_order.blade.php
@@ -1,7 +1,7 @@
 Hi {{$userName}},<br>
 <br>
 Your order <b>{{$orderTitle}}</b> has been delivered successfully by {{Auth::user()->name}}. <br>
-Please click <a href="http://skipthecafe.test/orders">here</a> to view your order. <br>
+Please click <a href="{{env('MAIN_PAGE_IP')}}">here</a> to view your order. <br>
 <br>
 Thanks,<br>
 Skip-the-Cafe
--- resources/views/emails/new_order.blade.php
+++ resources/views/emails/new_order.blade.php
@@ -1,7 +1,7 @@
 Hi {{ Auth::user()->name }},<br>
 <br>
 Your order <b>{{$orderTitle}}</b> has been successfully posted. <br>
-Please click <a href="http://skipthecafe.test/orders">here</a> to view your order. <br>
+Please click <a href="{{env('MAIN_PAGE_IP')}}">here</a> to view your order. <br>
 <br>
 Thanks,<br>
 Skip-the-Cafe
