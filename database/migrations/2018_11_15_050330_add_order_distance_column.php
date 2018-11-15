8e23d025f46697da16682013fb24fbe1fe2312f7
jyl49 <jyl49@sfu.ca>
2018-11-15 05:40:20 +0000

    added the function for calculating and storing distance between order taker and owner

---
+++ database/migrations/2018_11_15_050330_add_order_distance_column.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddOrderDistanceColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function($table) {
+          $table->decimal('distance',3, 2)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function($table) {
+          $table->$table->dropColumn('distance');
+        });
+
+    }
+}
