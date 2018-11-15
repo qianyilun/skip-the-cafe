9f92ecb5e1f7c9fedff27557bc76ca183c3784b5
Andrew <yya123@sfu.ca>
2018-11-14 22:49:32 -0800

    finished the base setup for admin page

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row" style="width: 100%; margin-top: 30px;">
+            <div class="col-md-12">
+                <h1>Welcome to Admin Page!</h1>
+            </div>
+        </div>
+    </div>
+@endsection
