cb8580528d6875b83e0ddef84669d0ddb556ffb5
Andrew <yya123@sfu.ca>
2018-11-17 22:35:11 -0800

    Added UI for edit order page

--- resources/views/orders/edit.blade.php
+++ resources/views/orders/edit.blade.php
@@ -4,12 +4,33 @@
     <div class="container">
         <h1>Edit Order</h1>
         <form method="post" action="/orders/{{$order->id}}">
+
             {{csrf_field()}}
+
             <input type="hidden" name="_method" value="PUT">
-            <input type="text" name="title" placeholder="Title" value="{{$order->title}}">
+            <div class="form-group">
+                <input class="form-control" type="text" name="title" placeholder="Title">
+            </div>
+            <div class="form-group">
+                <input class="form-control" type="text" name="item" placeholder="Item (e.g. a coffee)">
+            </div>
+            <div class="form-group">
+                <textarea class="form-control" name="description" id="" rows="5"  placeholder="Order Description"></textarea>
+            </div>
+            <div class="form-group">
+                <input class="form-control" type="textarea" name="address" placeholder="Address (Please be specific, e.g. SFU Burnaby campus library first floor)">
+            </div>
+            <div class="form-group">
+                <input class="form-control" type="number" name="price" placeholder="Price" step="0.01">
+            </div>
+
+            <input type="hidden" id="hiddenLongitude" value="" name="longitude" />
+            <input type="hidden" id="hiddenLatitude" value="" name="latitude" />
+
             {{csrf_field()}}
-            <input type="submit" name="UPDATE">
+            <input class="btn btn-primary" type="submit" name="UPDATE">
         </form>
+
         <form method="post" action="/orders/{{$order->id}}">
             {{csrf_field()}}
             <input type="hidden" name="_method" value="DELETE">
