c673f4798da7cabb18373ac72e209b272088f2a9
Andrew <yya123@sfu.ca>
2018-11-19 21:28:28 -0800

    Added edit user page for admin

--- routes/web.php
+++ routes/web.php
@@ -49,6 +49,7 @@ Route::get('/testadmin', 'AdminController@testAdmin')->name('testAdmin');
 Route::get('admin/user/{id}/orders', 'OrdersController@getUserOrders');
 Route::get('admin/user/{id}/grantadmin', 'AdminController@grantAdmin');
 Route::get('admin/user/{id}/edit', 'AdminController@editUser');
+Route::post('admin/user/{id}/update', 'AdminController@updateUser');
 
 /*
 |--------------------------------------------------------------------------
