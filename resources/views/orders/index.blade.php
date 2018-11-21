b1f48e2ea9938937902a705ea0cbd63320828ca0
Jacky <jacky-liu9@htoamil.com>
2018-11-20 20:35:44 -0800

    code clean

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -24,18 +24,6 @@
 </style>
 @section('content')
 {{-- Strictly for popup window --}}
-
-<div id="modal-container">
-  <div class="modal-background">
-    <div class="modal">
-      <h2>I'm a Modal</h2>
-      <p>Hear me roar.</p>
-      <svg class="modal-svg" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" preserveAspectRatio="none">
-								<rect x="0" y="0" fill="none" width="226" height="162" rx="3" ry="3"></rect>
-							</svg>
-    </div>
-  </div>
-</div>
 <div class="content">
 </div>
 
