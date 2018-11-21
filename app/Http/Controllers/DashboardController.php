e3f78fa15f0bc163f6db675c953ad8956ac850f9
Jacky <jacky-liu9@htoamil.com>
2018-11-20 17:20:37 -0800

    get data for weekly order count

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Order;
 use App\User;
+use Carbon\Carbon;
 
 class DashboardController extends Controller
 {    
@@ -20,6 +21,16 @@ class DashboardController extends Controller
         $user = auth()->user();
       }
 
+      $weekly_order_count = [0,1,2,3,4,5,6];
+      foreach ($weekly_order_count as $i){
+        $day = Carbon::now()->subDays($i);
+        $order_count= count(Order::whereDate('created_at', $day)->where('owner', $user->name)->get());
+        $weekly_order_count[6-$i] = $order_count;
+      };
+
+
+
+
 
       $ordersPostedByUser = Order::where('owner', $user->name)->get();
       $completedOrdersPostByUser = Order::where('owner', $user->name)->where('completed', true)->get();
@@ -29,7 +40,10 @@ class DashboardController extends Controller
       $completedOrdersTakenByUser = Order::where('taker', $user->id)->where('completed', true)->get();
 
 
-      return view('dashboard.index', compact('user', 'ordersPostedByUser', 'completedOrdersPostByUser', 'incompletedOrdersTakenByUser', 'completedOrdersTakenByUser', 'orders', 'currentUserlongitude', 'currentUserlatitude'));
+      return view('dashboard.index',
+             compact('user', 'ordersPostedByUser', 'completedOrdersPostByUser', 'incompletedOrdersTakenByUser', 
+                     'completedOrdersTakenByUser', 'orders', 'currentUserlongitude', 'currentUserlatitude',
+                     'weekly_order_count'));
 
     }
 }
